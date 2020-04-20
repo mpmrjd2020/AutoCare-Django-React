4d375b49d4324c1783af392e619908e325bc5f1e
Patrick Mutamba <mutambamilambop@johndeere.com>
2020-04-20 00:12:13 -0500

    updated setting

--- AutoCare_Django/settings.py
+++ AutoCare_Django/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ud)-v=c_ef#3z2w7ym*e7(*+xaxpq1%m3b11h_w*6hr8)6qrv0'
+# SECRET_KEY = 'ud)-v=c_ef#3z2w7ym*e7(*+xaxpq1%m3b11h_w*6hr8)6qrv0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gentle-stream-26777.herokuapp.com']
 
 
 # Application definition
